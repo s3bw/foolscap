0f44f498ec1064ec12187d9016d1d24b72b98e8d
GiantsLoveDeathMetal <sebastien@cytora.com>
2018-02-01 00:16:56 +0000

    Move load meta out of cli and into actor

--- foolscap/actor.py
+++ foolscap/actor.py
@@ -1,3 +1,4 @@
+from meta_data import load_data
 from migrate_data import update_version
 from note_display import list_notes
 from note_content import (
@@ -24,8 +25,10 @@ FUNCTION_MAP = {
     'migrate': update_version,
 }
 
+META_DATA = load_data()
 
-def action(do_action, meta_data, arg):
+
+def action(do_action, arg):
     func = FUNCTION_MAP[do_action]
 
     new_action = None
@@ -33,16 +36,16 @@ def action(do_action, meta_data, arg):
         # Quitting from list calls exit() method.
         # arg is filter in this case
         if arg:
-            new_action = func(arg, meta_data)
+            new_action = func(arg, META_DATA)
         else:
-            new_action = func(None, meta_data)
+            new_action = func(None, META_DATA)
 
     if new_action:
         new_func, note = new_action
-        action(new_func, meta_data, note)
+        action(new_func, note)
     # arg is note in this case
     elif arg:
-        func(arg, meta_data)
+        func(arg, META_DATA)
     else:
-        func(meta_data)
+        func(META_DATA)
 
