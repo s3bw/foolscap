62ef7f925cabba939f4572457ec5971700589889
GiantsLoveDeathMetal <sebastien@cytora.com>
2018-02-04 23:12:23 +0000

    Fix delete not removing meta data

--- foolscap/meta_data/__init__.py
+++ foolscap/meta_data/__init__.py
@@ -6,5 +6,6 @@ from foolscap.meta_data.common import shift_lines
 
 from foolscap.meta_data.common import note_exists
 from foolscap.meta_data.common import new_component
+from foolscap.meta_data.common import remove_component
 from foolscap.meta_data.common import update_component
 from foolscap.meta_data.common import upgrade_components
