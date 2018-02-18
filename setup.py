edf7f42f2bc21049957428a347494ff788bbfdfe
GiantsLoveDeathMetal <sebastien@cytora.com>
2018-02-18 01:38:02 +0000

    Bump to version 0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 
 setup(
     name='foolscap',
-    version='0.0.1',
+    version='0.1',
     author='GiantsLoveDeathMetal',
     author_email='s.williamswynn.mail@gmail.com',
     packages=find_packages(
