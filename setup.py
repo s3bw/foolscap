8845f45803d51c69f01b923730825fc712d0cb0a
Sebastien Williams-Wynn <s.williamswynn.mail@gmail.com>
2019-04-29 00:33:39 +0100

    Add vim-edit dependency, add py37 testing (#254)
        Add vim-edit dependency, add py37 testing
        Bump foolscap
    
        Resolves: #253

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-import os
-
 from setuptools import setup
 from setuptools import find_packages
 
 
 setup(
     name='foolscap',
-    version='0.3',
-    author='GiantsLoveDeathMetal',
+    version='0.3.1',
+    author='S. Williams-Wynn',
     author_email='s.williamswynn.mail@gmail.com',
     packages=find_packages(
         include=['foolscap', 'foolscap.*'],
@@ -18,6 +16,7 @@ setup(
             'fscap=foolscap.cli:main',
         ]
     },
+    install_requires=["vim-edit"],
     classifiers=[
         "Environment :: Console",
         "Operation System :: OS Independent",
